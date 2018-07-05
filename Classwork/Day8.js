a99fb3a2b11cad05abec807b33256f89c229214e
winno67843 <40576332+winno67843@users.noreply.github.com>
2018-07-05 13:52:30 -0400

    Assignments

---
+++ Classwork/Day8.js
@@ -0,0 +1,7 @@
+function changeParagraph() {
+	document.getElementById("demo").innerHTML="Hello JavaScript!";
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+	changeParagraph();
+});
